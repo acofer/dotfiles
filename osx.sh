493d5e8c87400333fd37d6148e5f0f7ec03d8f71
Adam Cofer <adam_cofer@yahoo.com>
2015-12-10 16:42:17 -0500

    Add bash profile to osx script

--- osx.sh
+++ osx.sh
@@ -33,9 +33,10 @@ ln -s /Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl ~/bin/subl
 echo "You probably want to press Ctrl+` in Sublime Text and then run this:"
 echo "import urllib.request,os,hashlib; h = '2915d1851351e5ee549c20394736b442' + '8bc59f460fa1548d1514676163dafc88'; pf = 'Package Control.sublime-package'; ipp = sublime.installed_packages_path(); urllib.request.install_opener( urllib.request.build_opener( urllib.request.ProxyHandler()) ); by = urllib.request.urlopen( 'http://packagecontrol.io/' + pf.replace(' ', '%20')).read(); dh = hashlib.sha256(by).hexdigest(); print('Error validating download (got %s instead of %s), please try manual install' % (dh, h)) if dh != h else open(os.path.join( ipp, pf), 'wb' ).write(by)"
 
-ln -s "./.bashrc" ~
-ln -s "./.tmux.conf" ~
-ln -s "./.vimrc" ~
+ln -s "~/dotfiles/.bash_profile" ~
+ln -s "~/dotfiles/.bashrc" ~
+ln -s "~/dotfiles/.tmux.conf" ~
+ln -s "~/dotfiles/.vimrc" ~
 
 mkdir ~/.ssh
 cat ./id_rsa.pub >> ~/.ssh/authorized_keys
